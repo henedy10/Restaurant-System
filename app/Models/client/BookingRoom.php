3fcfed188c3d4354ab7150a1095087e71f92af48
Ahmedf <ahmedfaisalzayed288@gmail.com>
2025-10-18 07:34:56 +0300

    handling with the function of deleting the reservation automatically when the reservation time expires

--- app/Models/client/BookingRoom.php
+++ app/Models/client/BookingRoom.php
@@ -3,8 +3,17 @@
 namespace App\Models\client;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\MassPrunable;
+use Illuminate\Database\Eloquent\Builder;
 
 class BookingRoom extends Model
 {
+    use MassPrunable;
+
     protected $guarded=[];
+
+    public function prunable(): Builder
+    {
+        return static::where('date', '<', now()->subDay());
+    }
 }
